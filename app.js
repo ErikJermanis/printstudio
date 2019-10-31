a6dd6abd0fa6043b093ecae3e658bbd06ef1db3e
ErikJermanis <erik55jermanis@gmail.com>
2019-10-31 19:56:43 +0100

    added first gen hamburger animation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.toggler');
 const navLinks = document.querySelector('.clip-box');
 const links = document.querySelectorAll('header ul li a');
+const line = document.querySelector('.line');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
+    line.classList.toggle('opena');
 });
 
 links.forEach(link => {
