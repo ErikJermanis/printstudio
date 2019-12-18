ed7b7cb8b1e77841066e4cd3a657d560b3cf50e1
ErikJermanis <erik55jermanis@gmail.com>
2019-12-18 13:21:35 +0100

    Click and drag engine reworked, everything works fine

--- katalozi/katalog1/katalog1.js
+++ katalozi/katalog1/katalog1.js
@@ -23,8 +23,47 @@ const turnBwd = () => {
 
 const toggleZoom = () => {
     if (zoom) {
-        document.querySelector(".bodyClass").style.overflow = 'scroll';
 
+        // Click and drag engine
+        const mag = document.querySelector('.wrapper');
+        mag.style.overflow = 'scroll';
+
+        let startY;
+        let startX;
+        let scrollLeft;
+        let scrollTop;
+        let isDown;
+
+        mag.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startY = e.pageY;
+            startX = e.pageX;
+            scrollLeft = mag.scrollLeft;
+            scrollTop = mag.scrollTop;
+        });
+
+        mag.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+
+        mag.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+
+        mag.addEventListener('mousemove', (e) => {
+            if(isDown) {
+                e.preventDefault();
+                const y = e.pageY;
+                walkY = y - startY;
+                mag.scrollTop = scrollTop - walkY;
+
+                const x = e.pageX;
+                const walkX = x - startX;
+                mag.scrollLeft = scrollLeft - walkX;
+            }
+        });
+
+        // Resizing stuff
         document.querySelectorAll('.page').forEach(single => {
             single.style.marginTop = '0px',
             single.style.marginLeft = '158vh',
@@ -46,29 +85,24 @@ const toggleZoom = () => {
 
         zoom = 0;
     } else {
-        document.querySelector(".bodyClass").style.overflow = 'hidden';
+        const mag = document.querySelector('.wrapper');
+        mag.style.overflow = 'hidden';
 
         document.querySelectorAll('.page').forEach(single => {
             single.style.marginTop = '10px',
             single.style.marginLeft = 'calc((100vw - 158vh) / 2 + 79vh)',
             window.innerWidth / window.innerHeight > 1.6 ? single.style.width = '79vh' : single.style.width = '49vw';
             window.innerWidth / window.innerHeight > 1.6 ? single.style.height = '95vh' : single.style.height = '59vw';
-            /*single.style.width = '79vh',
-            single.style.height = '95vh'*/
         });
 
         document.querySelectorAll('.pageFront').forEach(single => {
             window.innerWidth / window.innerHeight > 1.6 ? single.style.width = '79vh' : single.style.width = '49vw';
             window.innerWidth / window.innerHeight > 1.6 ? single.style.height = '95vh' : single.style.height = '59vw';
-            /*single.style.width = '79vh',
-            single.style.height = '95vh'*/
         });
 
         document.querySelectorAll('.pageBack').forEach(single => {
             window.innerWidth / window.innerHeight > 1.6 ? single.style.width = '79vh' : single.style.width = '49vw';
             window.innerWidth / window.innerHeight > 1.6 ? single.style.height = '95vh' : single.style.height = '59vw';
-            /*single.style.width = '79vh',
-            single.style.height = '95vh'*/
         });
 
         document.querySelector('.btnZoom').innerHTML = "Zumiraj";
@@ -76,35 +110,3 @@ const toggleZoom = () => {
         zoom = 1;
     }
 };
-
-const bodyObj = document.querySelector('.bodyClass')
-let isDown = false;
-let startX;
-let startY;
-let scrollX;
-let scrollY;
-
-bodyObj.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    isDown = true;
-    startX = e.pageX;
-    startY = e.pageY;
-    scrollX = window.scrollX;
-    scrollY = window.scrollY;
-});
-
-bodyObj.addEventListener('mouseleave', () => {});
-
-bodyObj.addEventListener('mouseup', () => {
-    isDown = false;
-});
-
-bodyObj.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX;
-    const y = e.pageY;
-    const walkX = x - startX;
-    const walkY = y - startY;
-    window.scroll(scrollX - walkX, scrollY - walkY);
-});
